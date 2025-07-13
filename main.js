307697c05a9ab4a571d8c58d8b0e2a02e8b64815
simaku0331 <t20070331@gmail.com>
2025-07-13 19:44:41 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const slides = document.querySelectorAll('#imgplay .slide');
+  let current = 0;
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.classList.toggle('active', i === index);
+    });
+  }
+
+  function nextSlide() {
+    current = (current + 1) % slides.length;
+    showSlide(current);
+  }
+
+  setInterval(nextSlide, 3000); // 每 3 秒切換
